11a8d8d8ccc5179183df0be596c8999fef24c90c
rahul-5503 <rahulkrishna5503@gmail.com>
2024-01-09 17:13:44 +0530

    first update  update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from 'react'
+import {loadStripe} from '@stripe/stripe-js';
+import {BrowserRouter , Route,Routes} from 'react-router-dom';
+import Home from './container/Home.js';
+import Success from './container/Success.js';
+import Fail from './container/Fail.js';
+const App = () => {
+  const [product, setProduct] = useState({
+    name: 'react',
+    price:100*2,
+    productBy:"rk",
+    desc:"it is good",
+    quantity: 5
+  });
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const moneyto =async()=>{
+  const stripe =await loadStripe('pk_test_51OWI9ASGskRZRKP1rE04BAPKFZaO3UNYxEJA2meN0nScNZi6WE2NCNktdztj2i1tT5Iitt68TuaEsUorFgXwp3Ga00OELN4zDO');
+
+  const body={
+    products:[product]
+  }
+  
+  const headers ={
+    "Content-Type":"application/json",
+    };
+  const response=await fetch(`${apiURL}/create-checkout-session`,{
+      method:'POST',
+      headers,
+      body:JSON.stringify(body)
+    })
+    const session=await response.json();
+    const result =stripe.redirectToCheckout({
+      sessionId:session.id
+    });
+    if(result.error){
+      console.log(result.error);
+    }
 }
 
-export default App;
+  return (
+    <BrowserRouter>
+     <Routes>
+     <Route path="/" element ={<Home/>}/>
+      <Route path="/Success" element={<Success/>}/>
+      <Route path="/Cancelled" element={<Fail/>} />
+     </Routes>
+    </BrowserRouter>
+  
+  )
+}
+
+export default App
